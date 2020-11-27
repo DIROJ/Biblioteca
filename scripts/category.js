a38c7f2c9734cb43a40a22efe0b648c903651236
DIROJ <69317292+DIROJ@users.noreply.github.com>
2020-11-26 20:19:32 -0600

    Add files via upload

---
+++ scripts/category.js
@@ -0,0 +1,11 @@
+function barra(){
+    document.getElementById('cuerpo').style.overflow = ('hidden');
+}
+function barraX(){
+    document.getElementById('cuerpo').style.overflowY = ('scroll');
+    document.getElementById('cuerpo').style.scroll = ('scroll');
+}
+
+
+     
+ 
