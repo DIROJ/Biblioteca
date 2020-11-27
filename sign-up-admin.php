07891e272b076abdadb410150e311e7fd1ba5d8d
DIROJ <69317292+DIROJ@users.noreply.github.com>
2020-11-26 20:18:31 -0600

    Add files via upload

---
+++ sign-up-admin.php
@@ -0,0 +1,37 @@
+<?php 
+$conexion = mysqli_connect("localhost", "Administrador", "9XRhqVbLCrSSnC1Z", "biblion" );
+
+$nombre       = $_POST["nombre"]; 
+$apellido     = $_POST["apellido"];
+$correo       = $_POST["correo"];
+$sexo         = $_POST["sexo"];
+$password     = $_POST["pass"];
+$correoc      = $_POST["correoc"];
+$nivel        = "Administrador";
+
+
+$val= "SELECT `Nivel` FROM `usuarios` WHERE Correo = '$correoc'";
+$ejecucion=mysqli_query($conexion, $val);
+$fila= mysqli_fetch_array ($ejecucion);
+
+
+
+if ($fila [0] == 'Administrador'){
+    $sql = "INSERT INTO `usuarios`(`Nombre`, `Apellido`, `Correo`, `Sexo`, `Password`, `Nivel`)
+     VALUES ('$nombre','$apellido','$correo','$sexo','$password', '$nivel')";
+
+    $ejecucion2=mysqli_query($conexion, $sql);
+
+    if($ejecucion2){
+        header('Location: sign-up-admin.html');
+    }else{
+        die (mysqli_error($conexion));
+    }
+}else{
+    header('location: plantilla.html');
+}
+
+
+?>
+
+
